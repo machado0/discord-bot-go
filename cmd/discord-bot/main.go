9311b2a5a7378c52afa84bcbcc82b7e4dbd574ce
machado0 <medmachado27@gmail.com>
2025-08-18 00:50:38 -0300

    feat: automated league ranked game lookup with routines

--- cmd/discord-bot/main.go
+++ cmd/discord-bot/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -14,21 +16,19 @@ import (
 	"discord-bot-go/internal/pkg/addbday"
 	"discord-bot-go/internal/pkg/addbdaychannel"
 	"discord-bot-go/internal/pkg/deletebday"
-	"discord-bot-go/internal/pkg/listbdays"
+	listbirthdays "discord-bot-go/internal/pkg/listbdays"
 	"discord-bot-go/internal/pkg/nextbday"
-	"discord-bot-go/internal/pkg/verifybday"
 	"discord-bot-go/internal/pkg/utils"
-	"discord-bot-go/internal/pkg/verifythumpysoloqueue"
+	"discord-bot-go/internal/pkg/verifybday"
+	"discord-bot-go/internal/pkg/verifythumpy"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/glebarez/sqlite"
-	"gorm.io/gorm"
 	"github.com/joho/godotenv"
+	"gorm.io/gorm"
 )
 
 // TODO: fazer o cadastrar canal funcionar
-
-var db, err = gorm.Open(sqlite.Open(os.Getenv("DB_NAME")), &gorm.Config{})
 
 var command_add string = "!adicionar"
 var command_delete string = "!remover"
@@ -40,16 +40,16 @@ var command_commands string = "!comandos"
 var command_verificar_solo_duo_luca string = "!soloduo"
 
 func main() {
-	db.AutoMigrate(&domain.Birthday{})
-
 	errorEnv := godotenv.Load()
-		if errorEnv != nil {
-			log.Fatalf("Erro carregando arquivo .env: %v", errorEnv)
-		}
+	if errorEnv != nil {
+		log.Fatalf("Erro carregando arquivo .env: %v", errorEnv)
+	}
+
+	var db, err = gorm.Open(sqlite.Open(os.Getenv("DB_NAME")), &gorm.Config{})
+	db.AutoMigrate(&domain.Birthday{}, &domain.LastMatch{}, &domain.Server{})
 
 	botToken := os.Getenv("BOT_TOKEN")
 	riotApiKey := os.Getenv("RIOT_API_KEY")
-	
 	riotClient := riot.NewRiotClient(riotApiKey)
 
 	sess, err := discordgo.New(fmt.Sprintf("Bot %s", botToken))
@@ -61,9 +61,7 @@ func main() {
 		if m.Author.ID == s.State.User.ID {
 			return
 		}
-
 		args := strings.Split(m.Content, " ")
-
 		switch args[0] {
 		case command_add:
 			addbday.Add(args, s, m, db, command_add)
@@ -80,30 +78,44 @@ func main() {
 		case command_commands:
 			utils.ListCommands(s, m)
 		case command_verificar_solo_duo_luca:
-			verifythumpysoloqueue.VerifyThumpySoloQueueCommand(s, m, riotClient)
+			verifythumpy.VerifyThumpyCommand(s, m, riotClient)
 		}
-
 	})
 
 	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
-
 	err = sess.Open()
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer sess.Close()
-	fmt.Println("bot online!")
 
-	dailyBirthdayCheck(sess)
+	fmt.Println("Bot online!")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		dailyBirthdayCheck(sess, db)
+	}()
+
+	go func() {
+		thumpyCheckEveryTwoMinutes(ctx, sess, db, riotClient)
+	}()
 
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	fmt.Println("Pressione Ctrl+C para parar o bot...")
 	<-sc
 
+	fmt.Println("Parando o bot...")
+	cancel()
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("Bot parado!")
 }
 
-func dailyBirthdayCheck(s *discordgo.Session) {
+func dailyBirthdayCheck(s *discordgo.Session, db *gorm.DB) {
 	targetHour := 9
 	targetMinute := 0
 	targetSecond := 0
@@ -119,7 +131,7 @@ func dailyBirthdayCheck(s *discordgo.Session) {
 		durationUntilNextRun := nextRun.Sub(now)
 		time.Sleep(durationUntilNextRun)
 
-		fmt.Println("rodou bot de aniversario:", time.Now())
+		fmt.Println("rodou rotina de aniversario:", time.Now())
 
 		var birthdays []domain.Birthday
 		result := db.Where("day = ? AND month = ?", now.Day(), int(now.Month())).Preload("Server").Find(&birthdays)
@@ -132,6 +144,84 @@ func dailyBirthdayCheck(s *discordgo.Session) {
 		for _, bday := range birthdays {
 			if len(bday.Server.BirthdayChannel) > 1 {
 				s.ChannelMessageSend(bday.Server.BirthdayChannel, fmt.Sprintf("@everyone 🎉 Hoje é aniversário de %s! 🎂", bday.User))
+			}
+		}
+	}
+}
+
+func thumpyCheckEveryTwoMinutes(ctx context.Context, s *discordgo.Session, db *gorm.DB, client *riot.RiotClient) {
+	ticker := time.NewTicker(2 * time.Minute)
+	defer ticker.Stop()
+
+	fmt.Println("Iniciando verificação thumpy")
+	thumpyPuuid := os.Getenv("THUMPY")
+
+	if thumpyPuuid == "" {
+		fmt.Println("ERRO: THUMPY puuid não configurado no .env")
+		return
+	}
+
+	var lastMatch domain.LastMatch
+	result := db.Where("puuid = ?", thumpyPuuid).First(&lastMatch)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			fmt.Println("Nenhuma partida anterior encontrada para Thumpy")
+
+			_, _, matchId := verifythumpy.GetThumpyLastSoloDuo(client)
+
+			if matchId != "" {
+				obj := domain.LastMatch{
+					MatchId: matchId,
+					Puuid:   thumpyPuuid,
+				}
+
+				if err := db.Create(&obj).Error; err != nil {
+					fmt.Printf("Erro ao criar registro inicial: %v\n", err)
+					return
+				}
+
+				lastMatch = obj
+				fmt.Println("Primeira partida registrada:", matchId)
+			} else {
+				fmt.Println("Não foi possível obter partida inicial")
+				return
+			}
+		} else {
+			fmt.Printf("Erro ao buscar última partida do Thumpy: %v\n", result.Error)
+			return
+		}
+	} else {
+		fmt.Println("Última partida conhecida:", lastMatch.MatchId)
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Parando verificação Thumpy")
+			return
+		case <-ticker.C:
+			fmt.Println("Verificando partidas do Thumpy:", time.Now().Format("15:04:05"))
+
+			_, matchResponse, currentMatchId := verifythumpy.GetThumpyLastSoloDuo(client)
+
+			if currentMatchId == "" {
+				fmt.Println("Erro ao obter partida atual da API")
+				continue
+			}
+
+			if currentMatchId != lastMatch.MatchId {
+				fmt.Printf("Nova partida detectada! Anterior: %s, Nova: %s\n", lastMatch.MatchId, currentMatchId)
+
+				if err := db.Model(&lastMatch).Update("match_id", currentMatchId).Error; err != nil {
+					fmt.Printf("Erro ao atualizar partida no banco: %v\n", err)
+					continue
+				}
+
+				lastMatch.MatchId = currentMatchId
+				fmt.Printf("Partida atualizada com sucesso: %s\n", currentMatchId)
+				verifythumpy.SendDiscordMessageAfterMatch(client, s, matchResponse, thumpyPuuid)
+			} else {
+				fmt.Println("Nenhuma nova partida encontrada")
 			}
 		}
 	}
